8f14995952c7067193e9a1fed68e965a6036363c
Jonathan <yoda1304@gmail.com>
2021-05-11 15:54:12 -0400

    added personalization features to the search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,9 @@ class HomeController < ApplicationController
 
         redirect_back(fallback_location: home_path) if params[:query].blank?
         @query = params[:query]
+        @user_courses =  current_user.courses
         @notes = Note.search(@query)
+        @course_notes = @notes.select{|n| !(@user_courses & n.courses).empty?}
         @users = User.search(@query)
         @total_hits = @notes.size + @users.size
 
--- app/models/course_user.rb
+++ app/models/course_user.rb
@@ -1,4 +1,7 @@
 class CourseUser < ApplicationRecord
   belongs_to :course
   belongs_to :user
+
+  scope :for_user, -> (user_id) {where(user_id: user_id)}
+
 end
